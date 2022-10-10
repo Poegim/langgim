dc4eb83ea34bde867afcca17409b1a1b38fa93f5
poegim <jacekwiniarczyk@gmail.com>
2022-10-10 11:08:45 +0200

    Minor design fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,7 +47,7 @@
 
             <!-- Page Content -->
             <main>
-                <div class="max-w-7xl mx-auto py-6 px-2 sm:px-6 lg:px-8">
+                <div class="max-w-7xl mx-auto py-6 px-2 sm:px-4 lg:px-8">
                 {{ $slot }}
                 </div>
             </main>
