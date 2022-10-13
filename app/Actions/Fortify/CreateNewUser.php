7b3e284d34ba537145c2a321a34c6b94f5e065e6
poegim <jacekwiniarczyk@gmail.com>
2022-10-13 12:24:34 +0200

    Register fix 6

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -22,11 +22,14 @@ class CreateNewUser implements CreatesNewUsers
     public function create(array $input)
     {
         Validator::make($input, [
+            'recaptcha_token' => ['required', new Recaptcha],
+        ])->validate();
+
+        Validator::make($input, [
             'name' => ['required', 'string', 'min:3', 'max:25'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
-            'recaptcha_token' => ['required', new Recaptcha],
         ])->validate();
 
         return User::create([
