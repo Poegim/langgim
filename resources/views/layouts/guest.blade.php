7f302228c3629f2c7df77541d89451440cb02407
poegim <jacekwiniarczyk@gmail.com>
2022-10-13 13:41:07 +0200

    Register capcha fix 17

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,59 +1,3 @@
-{{-- <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
-        <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
-
-        @if (App::environment('production'))
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-N5KFMQZ01K"></script>
-        <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-N5KFMQZ01K');
-        </script>
-        @endif
-
-        <script src="https://www.google.com/recaptcha/api.js?render=6Ldrr2wiAAAAALaBNlzaDc31aTl7177oV4W3-rrs"></script>
-
-
-    </head>
-    <body>
-        @stack('scripts')
-
-        <x-jet-banner />
-
-        <div class="min-h-screen bg-gray-100">
-
-            <!-- Page Content -->
-            <main>
-                <div class="max-w-7xl mx-auto py-6 px-2 sm:px-4 lg:px-8">
-                {{ $slot }}
-                </div>
-            </main>
-        </div>
-
-        @stack('modals')
-        @livewireScripts
-        @stack('scripts')
-
-    </body>
-</html> --}}
-
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -76,8 +20,16 @@
 
     </head>
     <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
+        <x-jet-banner />
+
+        <div class="min-h-screen bg-gray-100">
+
+            <!-- Page Content -->
+            <main>
+                <div class="max-w-7xl mx-auto py-6 px-2 sm:px-4 lg:px-8">
+                {{ $slot }}
+                </div>
+            </main>
         </div>
 
         @stack('scripts')
