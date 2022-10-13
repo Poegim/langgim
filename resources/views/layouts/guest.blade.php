42346df271e36cd87d4dd06c6097be0cda2d5b93
poegim <jacekwiniarczyk@gmail.com>
2022-10-13 10:47:07 +0200

    Recaptcha fixes, app to guest layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -26,19 +26,29 @@
 
         gtag('config', 'G-N5KFMQZ01K');
         </script>
-    @endif
+        @endif
 
-    <script src="https://www.google.com/recaptcha/api.js?render={{ config('services.recaptcha.site_key') }}"></script>
+        <script src="https://www.google.com/recaptcha/api.js?render=6Ldrr2wiAAAAALaBNlzaDc31aTl7177oV4W3-rrs"></script>
 
 
     </head>
     <body>
         @stack('scripts')
 
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
+        <x-jet-banner />
+
+        <div class="min-h-screen bg-gray-100">
+
+            <!-- Page Content -->
+            <main>
+                <div class="max-w-7xl mx-auto py-6 px-2 sm:px-4 lg:px-8">
+                {{ $slot }}
+                </div>
+            </main>
         </div>
 
+        @stack('modals')
+        @livewireScripts
         @stack('scripts')
 
     </body>
