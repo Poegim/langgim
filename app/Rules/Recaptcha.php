8fd970cf6657ea54723a3162b766fdcd2ff7fab0
poegim <jacekwiniarczyk@gmail.com>
2022-10-13 13:35:01 +0200

    Register pcatcha fix 15

--- app/Rules/Recaptcha.php
+++ app/Rules/Recaptcha.php
@@ -2,49 +2,28 @@
 
 namespace App\Rules;
 
-use Illuminate\Contracts\Validation\Rule;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Contracts\Validation\Rule;
 
 class Recaptcha implements Rule
 {
-    /**
-     * Create a new rule instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
-     * Determine if the validation rule passes.
-     *
-     * @param  string  $attribute
-     * @param  mixed  $value
-     * @return bool
-     */
     public function passes($attribute, $value)
     {
         $response = Http::asForm()->post("https://www.google.com/recaptcha/api/siteverify", [
-            'secret' => config('services.recaptcha.secret_key'),
-            'response' => $value
+                'secret' => config('services.recaptcha.secret_key'),
+                'response' => $value,
+                'ip' => request()->ip(),
         ]);
 
-        if ($response->successful() && $response->json('success') && $response->json('score') > 0.5) {
+        if ($response->successful() && $response->json('success') && $response->json('score') > config('services.recaptcha.min_score')) {
             return true;
         }
 
         return false;
     }
 
-    /**
-     * Get the validation error message.
-     *
-     * @return string
-     */
     public function message()
     {
-        return 'Failed captcha validation.';
+        return 'Failed to validate ReCaptcha.';
     }
 }
