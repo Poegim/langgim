c7bbe8ba5d6d72a2964764d7d168e00d8b0fb4c2
poegim <jacekwiniarczyk@gmail.com>
2022-10-13 12:10:22 +0200

    Resgister fix test

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -23,10 +23,10 @@ class CreateNewUser implements CreatesNewUsers
     {
         Validator::make($input, [
             'name' => ['required', 'string', 'min:3', 'max:25'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
-            'recaptcha_token' => ['required', new Recaptcha($input['recaptcha_token'])],
+            // 'recaptcha_token' => ['required', new Recaptcha($input['recaptcha_token'])],
         ])->validate();
 
         return User::create([
