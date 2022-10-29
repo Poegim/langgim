4c723584ddb28a312804a1482faf6630467b449f
poegim <jacekwiniarczyk@gmail.com>
2022-10-29 12:26:30 +0200

    Minor landing design fixes

--- resources/views/guest.blade.php
+++ resources/views/guest.blade.php
@@ -13,22 +13,30 @@
                     @else
                     <div class="flex justify-center">
                         <div class="w-full sm:max-w-md px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                            <div class="mb-2">
-                                <span class="text-2xl">
+
+                            <div class="mb-4 flex justify-center">
+                                <span class="text-xl">
                                     Choose your language:
                                 </span>
                             </div>
-                            <div class="flex justify-center space-x-4 mb-2">
-                                @foreach (config('langgim.allowed_languages') as $language)
-                                <div>
-                                    <a href="{{route('guest', $language)}}">
-                                        <img src="{{asset('/images/flags/'.$language.'.svg')}}"
-                                        class="w-24 hover:opacity-90 rounded-md">
-                                    </a>
-                                </div>
-                                @endforeach
 
+                            @foreach (config('langgim.allowed_languages') as $language)
+                            <div class="flex justify-around">
+                                <div class="flex space-x-4 mb-4">
+                                    <div class="shadow">
+                                        <a href="{{route('guest', $language)}}" class="flex relative hover:opacity-90">
+                                            <img src="{{asset('/images/flags/'.$language.'.svg')}}"
+                                            class="w-24 rounded-md shadow z-30">
+                                            <img src="{{asset('/images/flags/polish.svg')}}"
+                                            class="w-24 rounded-md -right-16 absolute shadow hover:none">
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="mt-6">
+                                    <span class="h-1 font-bold">{{ucfirst($language)}} - Polish</span>
+                                </div>
                             </div>
+                            @endforeach
                         </div>
                     </div>
                     @endif
