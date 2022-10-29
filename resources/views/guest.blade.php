fafaafd51cf87a0bcc04e385cce517e96d91aacd
poegim <jacekwiniarczyk@gmail.com>
2022-10-29 10:00:55 +0200

    Guest mode design fix

--- resources/views/guest.blade.php
+++ resources/views/guest.blade.php
@@ -6,41 +6,29 @@
     </x-slot>
 
     <div class="py-2 sm:py-6">
-
-        <div class="mt-10 flex flex-col sm:justify-center items-center sm:pt-0 bg-gray-100">
-
-            <div>
-                    <span class="text-2xl">
-                        Choose your language:
-                    </span>
-            </div>
-
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                <div class="flex justify-center">
-
-                    <div class="font-thin">
-                        <div class="overflow-hidden sm:rounded-lg">
-                            @if ((!$language == NULL) && in_array($language, config('langgim.allowed_languages')))
-                                <livewire:writing :language="$language">
-                            @else
-                            <div class="flex justify-center space-x-4">
-                                @foreach (config('langgim.allowed_languages') as $language)
-                                    <div>
-                                        <a href="{{route('guest', $language)}}">
-                                            <img src="{{asset('/images/flags/'.$language.'.svg')}}" class="w-24 hover:opacity-90 rounded-md">
-                                        </a>
-                                    </div>
-                                @endforeach
-                            </div>
-                            @endif
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-
+            <div class="overflow-hidden sm:rounded-lg">
+                @if ((!$language == NULL) && in_array($language, config('langgim.allowed_languages')))
+                <livewire:writing :language="$language">
+                    @else
+                    <div>
+                        <span class="text-2xl">
+                            Choose your language:
+                        </span>
+                    </div>
+                    <div class="flex justify-center space-x-4">
+                        @foreach (config('langgim.allowed_languages') as $language)
+                        <div>
+                            <a href="{{route('guest', $language)}}">
+                                <img src="{{asset('/images/flags/'.$language.'.svg')}}"
+                                    class="w-24 hover:opacity-90 rounded-md">
+                            </a>
+                        </div>
+                        @endforeach
+                    </div>
+                    @endif
+            </div>
         </div>
     </div>
+
 </x-app-layout>
