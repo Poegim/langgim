23009517e5b0648385735ecfc06487209a60dc30
poegim <jacekwiniarczyk@gmail.com>
2023-08-22 13:07:08 +0200

    Guest dashboard design changes

--- resources/views/livewire/landing.blade.php
+++ resources/views/livewire/landing.blade.php
@@ -1,35 +1,37 @@
-<div class="mt-10 flex flex-col sm:justify-center items-center sm:pt-0">
-    <div>
-        <h1 class="text-2xl">
-            Welcome to LearnPolish.eu,
-        </h1>
-        <span class="font-thin text-md tracking-wider">
-            simple and free app for learning Polish.
-        </span>
-    </div>
-
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4">
-        <div class="text-center">
-            <a href="{{route('register')}}">
-                <x-buttons.primary class="w-full h-16">
-                    <div>Register</div>
-                </x-buttons.primary>
-            </a>
+<div class="flex flex-col sm:justify-center items-center">
+    <div class="mt-10 py-10 bg-slate-800 rounded-lg w-screen max-w-md">
+        <div class="w-full text-center">
+            <h1 class="text-2xl md:text-3xl">
+                Welcome to LearnPolish.eu,
+            </h1>
+            <span class="font-thin text-md tracking-wider">
+                simple and free app for learning Polish.
+            </span>
         </div>
 
-        <div class="mt-6">
-            <a href="{{route('login')}}">
-                <x-buttons.secondary class="w-full h-16">
-                    <div>Log in</div>
-                    </x-buttons.secondary>
-            </a>
-        </div>
-        <div class="mt-6">
-            <a href="{{route('guest')}}">
-                <x-buttons.third class="w-full h-16">
-                    <div>Guest mode</div>
-                    </x-buttons.third>
-            </a>
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4">
+            <div class="text-center">
+                <a href="{{route('register')}}">
+                    <x-buttons.primary class="w-full h-16 md:text-lg">
+                        <div>Register</div>
+                    </x-buttons.primary>
+                </a>
+            </div>
+
+            <div class="mt-6">
+                <a href="{{route('login')}}">
+                    <x-buttons.secondary class="w-full h-16 md:text-lg">
+                        <div>Log in</div>
+                        </x-buttons.secondary>
+                </a>
+            </div>
+            <div class="mt-6">
+                <a href="{{route('guest')}}">
+                    <x-buttons.third class="w-full h-16 md:text-lg">
+                        <div>Guest mode</div>
+                        </x-buttons.third>
+                </a>
+            </div>
         </div>
     </div>
 </div>
