a4bdd6e43522fedd66ebd6480b1bcb62290ede0c
poegim <jacekwiniarczyk@gmail.com>
2023-10-08 11:43:43 +0200

    Guest category index design fix, now looks same as for logged users

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -81,46 +81,38 @@
                 @guest
                 @if($category->this_language_words != 0)
 
-                <div class="overflow-hidden border-b border-slate-700 bg-slate-800" x-data="{ open_two: false }">
+                <div class="overflow-hidden border-b border-slate-700 hover:bg-blue-500 bg-slate-800" x-cloak x-data="{ open: false }">
                     <div class="flex justify-start min-w-min overflow-hidden">
-                        <button type="button" x-cloak x-on:click="open_two = ! open_two" :class="open_two ? '' : 'rotate-180'" class="mx-auto p-1 text-gray-300 hover:text-pink-600 transition-all duration-500">
-                            <x-heroicon-s-chevron-up class="w-6 h-6"/>
+                        <button type="button" x-on:click="open = ! open" :class="open ? '' : 'rotate-180'" class="mx-auto p-1 text-gray-300 hover:text-pink-600 transition-all duration-500">
+                            <x-heroicon-s-chevron-up class="w-6 h-6 rounded-full bg-slate-900 p-1"/>
                         </button>
-                        @if ($category->{$language} != NULL)
-                            <div class="my-auto ml-2 cursor-pointer" x-data="{ tooltip: false }"
-                            x-on:mouseover="tooltip = true" x-on:mouseleave="tooltip = false"
-                            class="cursor-pointer">
 
-                            <img src="{{asset('images/flags/'.$language.'.svg')}}" alt="{{$language}}" class="w-8 h-8 my-auto">
-
-                                <div x-show="tooltip" x-cloak class="pl-2 text-sm text-gray-200 absolute bg-blue-400 rounded-lg p-2
-                                transform translate-y-2">
-                                    {{ $category->{$language} }}
-                                </div>
-                            </div>
-                        @endif
-
-                        <div class="bg-slate-800 w-full pl-2 py-2 flex space-x-2">
-                            <a href="{{route('category.show', [$category])}}" class="w-full hover:text-white hover:ml-1 transition-all duration-100 hover:font-bold">
+                        <a href="{{route('category.show', [$category])}}" class="w-full hover:text-white hover:ml-1 transition-all duration-100 hover:font-bold p-2">
+                            <div class="w-full">
                                 <p>
                                     <h1 class="text-sm sm:text-xl uppercase">{{$category->name}}</h1>
                                 </p>
-                            </a>
-                        </div>
-
+                                <div class="text-xs sm:text-sm italic">
+                                    {{ $category->{$language} }}
+                                </div>
+                            </div>
+                        </a>
                     </div>
 
-                    <div x-show="open_two" x-cloak x-collapse.duration.500ms>
+                    <div x-show="open" x-cloak x-collapse.duration.500ms>
                         @foreach ($category->subcategories as $subcategory)
                         @if ($subcategory->this_language_words != 0)
                         <div class="flex justify-start min-w-min bg-slate-900">
-                            <div class=" w-full px-6 py-2 flex space-x-2">
-                                <a href="{{route('category.show', [$category, $subcategory])}}" class="w-full hover:text-white hover:font-bold transition-all duration-100">
+                            <a href="{{route('category.show', [$category, $subcategory])}}" class="hover:bg-slate-700 w-full hover:text-white hover:font-bold transition-all duration-100 p-2">
+                                <div class="w-full">
                                     <p>
                                         <h1 class="text-sm sm:text-lg">{{$subcategory->name}}</h1>
                                     </p>
-                                </a>
-                            </div>
+                                </div>
+                                <div class="text-xs sm:text-sm italic">
+                                    {{ $subcategory->{$language} }}
+                                </div>
+                            </a>
                         </div>
                         @endif
                         @endforeach
